9aed5ee7ef5093b22e495888b08b2eeec2a41c3b
alexmercerind <saini123hitesh@gmail.com>
2021-07-08 01:32:26 +0530

    Changed namespace to wm

--- examples/player.cpp
+++ examples/player.cpp
@@ -4,26 +4,23 @@
 #include <string>
 
 
-using namespace WinMedia;
-using namespace std;
+using namespace wm;
 
 int32_t main(int argc, const char* argv[]) {
     if (argc < 2) {
-        cout << "No media provided.\n"
-        << "Example Usage:\n"
-        << argv[0] << " file://C:/Music/MySong.mp3\n"
-        << argv[0] << " https://www.example.com/MyVideo.mp4\n";
+        std::wcout << L"No media provided.\n"
+        << L"Example Usage:\n"
+        << argv[0] << L" file://C:/Music/MySong.mp3\n"
+        << argv[0] << L" https://www.example.com/MyVideo.mp4\n";
         return EXIT_FAILURE;
     }
-    // Create a `Media`.
-    string uri(argv[1]);
-    Media media = Media(wstring(uri.begin(), uri.end()));
-    // Create a new `Player` & open `Media` in it.
+    std::string uri(argv[1]);
+    Media media = Media(std::wstring(uri.begin(), uri.end()), true);
+    std::cout << "Media duration: " << media.duration << " milliseconds.\n";
     Player player = Player();
     player.open(media);
     player.play();
-    cout << "Playing...\n";
-    // Prevent console from closing.
-    cin.get();
+    std::wcout << L"Playing " << argv[1] << " ...\n";
+    std::cin.get();
     return EXIT_SUCCESS;
 }
