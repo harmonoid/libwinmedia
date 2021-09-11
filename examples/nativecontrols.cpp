2dda9b4ca0d8d21c58c22a148016ef82a8a9bfb0
alexmercerind <saini123hitesh@gmail.com>
2021-09-11 11:50:17 +0530

    Update examples

--- examples/nativecontrols.cpp
+++ examples/nativecontrols.cpp
@@ -3,19 +3,16 @@
 #include <filesystem>
 #include "../include/libwinmedia.hpp"
 
-auto TO_WIDESTRING = [](std::string string) -> std::wstring {
-  return std::wstring(string.begin(), string.end());
-};
-
 int main(int ac, const char** av) {
   auto controls = lwm::NativeControls([](auto button) -> void {
-    std::wcout << L"Pressed button : " << button << L".\n";
+    std::wcout << "Pressed button : " << button << ".\n";
   });
-  std::wstring album_art = TO_WIDESTRING(
-      "file://" + std::filesystem::current_path().string() + "/AlbumArt.PNG");
+  auto album_art =
+      "file://" + std::filesystem::current_path().string() + "/AlbumArt.PNG";
+  std::cout << album_art << std::endl;
   controls.Update(std::make_shared<lwm::MusicNativeControlState>(
-                      L"album_artist", L"album", L"track_count", L"artist",
-                      L"title", L"track_number"),
+                      "album_artist", "album", "track_count", "artist", "title",
+                      "track_number"),
                   album_art);
   std::cin.get();
   return EXIT_SUCCESS;
--- examples/player.cpp
+++ examples/player.cpp
@@ -2,26 +2,22 @@
 #include <string>
 #include "../include/libwinmedia.hpp"
 
-auto TO_WIDESTRING = [](const char* array) -> std::wstring {
-  std::string string(array);
-  return std::wstring(string.begin(), string.end());
-};
-
-int main(int ac, const char** av) {
+int32_t main(int32_t ac, const char** av) {
   using namespace std;
   using namespace lwm;
   if (ac < 2) {
-    wcout << L"No URI provided.\n" << L"Example Usage:\n" << av[0]
-          << L" file://C:/alexmercerind/music.mp3\n" << av[0]
-          << L" https://alexmercerind.github.io/video.mp4\n";
+    cout << "No URI provided.\n"
+         << "Example Usage:\n" << av[0]
+         << " file://C:/alexmercerind/music.mp3\n" << av[0]
+         << " https://alexmercerind.github.io/video.mp4\n";
     return EXIT_FAILURE;
   }
   auto player = Player(0);
-  auto media = Media(TO_WIDESTRING(av[1]));
+  auto media = Media(av[1]);
   player.Open(vector<Media>{media});
   player.Play();
-  player.events()->Position([](int position) -> void {
-    wcout << L"Current playback position is " << position << L" ms.\n";
+  player.events()->Position([](int32_t position) -> void {
+    cout << "Current playback position is " << position << " ms.\n";
   });
   cin.get();
   return EXIT_SUCCESS;
